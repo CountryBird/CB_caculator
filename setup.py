5ff5180ca5563ccb54200ff7076a75984ab93d14
CountryBird <100738390+CountryBird@users.noreply.github.com>
2022-05-04 09:24:19 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name= 'CB_calculator',
+  version='0.0.1',
+  description='oss-dev CB calculator',
+  author='CountryBird',
+  author_email='ywsywz123@naver.com',
+  url='https://github.com/CountryBird/CB_caculator',
+  download_url #아직 안적어도 됨,
+  install_requires=['pytest'],
+  long_description='oss-dev my calc'
+  long_description_content_type='text/markdown',
+  classfiers=[
+    "Programming Language :: Python ::3",
+    "Operating System :: OS Independent",
+  ],
+)
